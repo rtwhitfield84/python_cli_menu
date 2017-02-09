5eaa4943b09c5c4323db30994aef512ff37ae3b6
Rico <richardthomaswhitfield@gmail.com>
2017-02-08 21:24:49 -0600

    initial menu options

---
+++ menu.py
@@ -0,0 +1,7 @@
+def mainMenu():
+	print("1. Do something good")
+	print("2. Do something bad")
+	print("3. Do something weird")
+	print("4. Do something funny")
+	print("5. Quit")
+	choice=int(input("Enter choice: "))
